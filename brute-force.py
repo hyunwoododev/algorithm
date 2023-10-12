8d5d3cad6f60486850c4cf91f6d5a396fd0ce429
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2023-10-13 00:43:55 +0900

    chapter 1.

---
+++ brute-force.py
@@ -0,0 +1,20 @@
+
+
+n,m = list(map(int,input().split()))
+ 
+s = []
+ 
+def dfs():
+    print(s)
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
+
+dfs()
