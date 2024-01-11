3e411132ffb0d7d7f1f312cb2848301a02dae607
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-01-11 17:40:12 +0900

    feat: graph

---
+++ python/brute-force/테크노미로.py
@@ -0,0 +1,76 @@
+"""
+14500번 문제
+
+폴리오미노란 크기가 1×1인 정사각형을 여러 개 이어서 붙인 도형이며, 다음과 같은 조건을 만족해야 한다.
+
+* 정사각형은 서로 겹치면 안 된다.
+* 도형은 모두 연결되어 있어야 한다.
+* 정사각형의 변끼리 연결되어 있어야 한다. 즉, 꼭짓점과 꼭짓점만 맞닿아 있으면 안 된다.
+정사각형 4개를 이어 붙인 폴리오미노는 테트로미노라고 하며, 다음과 같은 5가지가 있다.
+...
+"""
+
+import sys
+input = sys.stdin.readline
+
+# 상, 하, 좌, 우
+move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+# INPUT
+N, M = map(int, input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+
+# 최대값 변수
+maxValue = 0
+
+# ㅗ, ㅜ, ㅓ, ㅏ 제외한 모양들 최대값 계산
+def dfs(i, j, dsum, cnt):
+    global maxValue
+    # 모양 완성되었을 때 최대값 계산
+    if cnt == 4:
+        maxValue = max(maxValue, dsum)
+        return
+
+    # 상, 하, 좌, 우로 이동
+    for n in range(4):
+        ni = i+move[n][0]
+        nj = j+move[n][1]
+        if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj]:
+            # 방문 표시 및 제거
+            visited[ni][nj] = True
+            dfs(ni, nj, dsum + board[ni][nj], cnt+1)
+            visited[ni][nj] = False
+
+
+# ㅗ, ㅜ, ㅓ, ㅏ 모양의 최대값 계산
+def exce(i, j):
+    global maxValue
+    for n in range(4):
+        # 초기값은 시작지점의 값으로 지정
+        tmp = board[i][j]
+        for k in range(3):
+            # move 배열의 요소를 3개씩 사용할 수 있도록 인덱스 계산
+            # 012, 123, 230, 301
+            t = (n+k)%4
+            ni = i+move[t][0]
+            nj = j+move[t][1]
+
+            if not (0 <= ni < N and 0 <= nj < M):
+                tmp = 0
+                break
+            tmp += board[ni][nj]
+        # 최대값 계산
+        maxValue = max(maxValue, tmp)
+
+
+for i in range(N):
+    for j in range(M):
+        # 시작점 visited 표시
+        visited[i][j] = True
+        dfs(i, j, board[i][j], 1)
+        visited[i][j] = False
+
+        exce(i, j)
+
+print(maxValue)
