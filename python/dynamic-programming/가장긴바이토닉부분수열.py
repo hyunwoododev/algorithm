3d129c92433d800c71af26df6ce56042f772b3b4
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-01-18 18:31:03 +0900

    feat: dynamic programing

---
+++ python/dynamic-programming/가장긴바이토닉부분수열.py
@@ -0,0 +1,35 @@
+"""
+11054번
+
+수열 S가 어떤 수 Sk를 기준으로 S1 < S2 < ... Sk-1 < Sk > Sk+1 > ... SN-1 > SN을 만족한다면, 
+그 수열을 바이토닉 수열이라고 한다.
+예를 들어, {10, 20, 30, 25, 20}과 {10, 20, 30, 40}, {50, 40, 25, 10} 은 바이토닉 수열이지만, 
+{1, 2, 3, 2, 1, 2, 3, 2, 1}과 {10, 20, 30, 40, 20, 30} 은 바이토닉 수열이 아니다.
+수열 A가 주어졌을 때, 그 수열의 부분 수열 중 바이토닉 수열이면서 가장 긴 수열의 길이를 구하는 프로그램을 작성하시오.
+첫째 줄에 수열 A의 크기 N이 주어지고, 둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ N ≤ 1,000, 1 ≤ Ai ≤ 1,000)
+첫째 줄에 수열 A의 부분 수열 중에서 가장 긴 바이토닉 수열의 길이를 출력한다.
+"""
+
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+
+case = list(map(int, input().split()))
+reverse_case = case[::-1]
+
+increase = [1 for _ in range(x)] # 가장 긴 증가하는 부분 수열
+decrease = [1 for _ in range(x)] # 가장 긴 감소하는 부분 수열(reversed)
+
+for i in range(x):
+    for j in range(i):
+        if case[i] > case[j]:
+            increase[i] = max(increase[i], increase[j]+1)
+        if reverse_case[i] > reverse_case[j]:
+            decrease[i] = max(decrease[i], decrease[j]+1)
+
+result = [0 for i in range(x)]
+for i in range(x):
+    result[i] = increase[i] + decrease[x-i-1] -1
+
+print(max(result))
---
+++ python/dynamic-programming/가장큰증가하는수열.py
@@ -0,0 +1,26 @@
+"""
+11055번 문제
+수열 A가 주어졌을 때, 그 수열의 증가하는 부분 수열 중에서 합이 가장 큰 것을 구하는 프로그램을 작성하시오.
+
+예를 들어, 수열 A = {1, 100, 2, 50, 60, 3, 5, 6, 7, 8} 인 경우에 
+합이 가장 큰 증가하는 부분 수열은 A = {1, 100, 2, 50, 60, 3, 5, 6, 7, 8} 이고, 합은 113이다.
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
+둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+첫째 줄에 수열 A의 합이 가장 큰 증가하는 부분 수열의 합을 출력한다.
+"""
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int,input().split()))
+dp = [1]* N
+dp[0] = arr[0]
+
+for i in range(N):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j]+arr[i])
+        else:
+            dp[i] = max(arr[i], dp[i])
+
+print(max(dp))
