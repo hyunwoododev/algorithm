9e8d42345077f518734322921c443477ac133c99
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-02-29 02:11:48 +0900

    Update 46.가장 긴 팰린드롬.py

--- python/loop/46.가장 긴 팰린드롬.py
+++ python/loop/46.가장 긴 팰린드롬.py
@@ -1,2 +1,21 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12904
 
+def palindrome(s):
+    if len(s) < 2 or s == s[::-1]:
+        return True
+
+def solution(s):
+
+    if len(s) < 2 or s == s[::-1]:
+        return len(s)
+
+    for cur in range(len(s), 0, -1): # 즉, cur는 부분 문자열의 길이
+        for start in range(len(s)): 
+            currS = s[start: start + cur]
+
+            if palindrome(currS):
+                return len(currS)
+            
+            if start + cur > len(s):
+                break
+
