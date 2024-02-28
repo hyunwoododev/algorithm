6744620eaf6fe1609c8c4e8ae4f1c48fcb08a940
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-02-28 21:25:32 +0900

    Update 45.Highest Value Palindrome.py

--- python/loop/45.Highest Value Palindrome.py
+++ python/loop/45.Highest Value Palindrome.py
@@ -3,23 +3,22 @@
 n, k = map(int, input().split())
 s = list(input())
 f = 0
-for i in range(n // 2): 
+for i in range(n // 2):
     f += s[i] != s[n - 1 - i]
 
-if f > k: 
+if f > k:
     print(-1)
 else:
     for i in range(n // 2):
+        if s[i] == s[n - 1 - i] == '9':
+            continue
 
+        if k <= f + 1 and s[i] == s[n - 1 - i]:
+            continue
+            
         if s[i] != s[n - 1 - i]:
             f -= 1
-        
-        if s[i] == s[n - 1 - i] == '9': 
-            continue
-
-        if k == f + 1 and s[i] == s[n - 1 - i] :
-            continue
-        
+            
         if k - f > 1:
             k -= (s[i] != '9') + (s[n - 1 - i] != '9')
             s[i] = s[n - 1 - i] = '9'
