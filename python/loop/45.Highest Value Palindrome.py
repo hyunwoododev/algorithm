15ab5fe4d9f147e918ec3bad205abd9c74ed123f
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-02-28 16:56:19 +0900

    Feat: I cannot believe it..

---
+++ python/loop/45.Highest Value Palindrome.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/richie-rich/problem?isFullScreen=true
+
+n, k = map(int, input().split())
+s = list(input())
+f = 0
+for i in range(n // 2): 
+    f += s[i] != s[n - 1 - i]
+
+if f > k: 
+    print(-1)
+    
+else:
+    for i in range(n // 2):
+
+        if s[i] != s[n - 1 - i]:
+            f -= 1
+        
+        if k == f or s[i] == s[n - 1 - i] == '9': 
+            continue
+
+        if k == f + 1 and s[i] == s[n - 1 - i] :
+            continue
+        
+        if k  > f + 1:
+            k -= (s[i] != '9') + (s[n - 1 - i] != '9')
+            s[i] = s[n - 1 - i] = '9'
+
+        else:
+            s[i] = s[n - 1 - i] = max(s[i], s[n - 1 - i])
+            k -= 1
+
+    # n이 홀수이고 바꿀 수 있는 기회가 있으면 '9'로 바꿔
+    if n % 2 == 1 and k > 0:
+        s[n // 2] = '9'
+
+    print(''.join(s))
