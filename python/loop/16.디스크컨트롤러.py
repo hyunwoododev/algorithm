0a38d791d7ceff441ff98224d486c17798f88569
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-03-03 20:49:01 +0900

    feat: 많이 늘었다~

--- python/loop/16.디스크컨트롤러.py
+++ python/loop/16.디스크컨트롤러.py
@@ -4,32 +4,34 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42627
 import heapq
 
 def solution(jobs):
-    answer = 0
-    #처리해야할 job의 갯수
-    jobCnt = len(jobs)
-    #처리된 job의 갯수
-    completedCnt = 0
-    heap = []
-    now = 0  
-    completedTime= -1
+    answer = 0  # 모든 작업을 완료하는데 걸린 총 시간
+    now = 0  # 현재 시간
+    completedJob = 0  # 완료된 작업 수
+    jobCnt = len(jobs)  # 전체 작업 수
     
-    while jobCnt > completedCnt:
-        #처리해야할 job들을 추가하는 배열.
-        for start, processingTime in jobs:
-            if completedTime < start <= now:
-                heapq.heappush(heap,(processingTime, start))
-                
-        #처리해야할 job하나를 처리함.
-        if heap:
-            processingTime, start = heapq.heappop(heap)
-            #처리시작
-            completedTime = now
-            now += processingTime  
-            #처리완료
-            completedCnt += 1
-            answer += now - start
+    jobHeap = []  # 작업을 시작 시간 기준으로 정렬하기 위한 힙
+    for s, p in jobs:
+        # 작업을 (시작 시간, 처리 시간)의 튜플로 jobHeap에 추가
+        heapq.heappush(jobHeap, (s, p))
+    
+    tmpHeap = []  # 현재 시간에서 처리할 수 있는 작업들을 처리 시간 기준으로 정렬하기 위한 힙
+    
+    while completedJob < jobCnt:  # 모든 작업이 완료될 때까지 반복
+        # 현재 시간 이하에서 시작 가능한 모든 작업을 tmpHeap으로 옮김
+        while jobHeap and jobHeap[0][0] <= now:
+            start, processingTime = heapq.heappop(jobHeap)
+            # 처리 시간을 기준으로 tmpHeap에 작업 추가
+            heapq.heappush(tmpHeap, (processingTime, start))
+        
+        if tmpHeap:
+            # 처리 시간이 가장 짧은 작업을 tmpHeap에서 꺼냄
+            processingTime, start = heapq.heappop(tmpHeap)
+            now += processingTime  # 현재 시간 업데이트
+            answer += now - start  # 대기 시간 포함하여 총 시간에 추가
+            completedJob += 1  # 완료된 작업 수 업데이트
         else:
-            #아직 처리할 job이 없으면 now 이동
+            # 처리할 작업이 없으면 현재 시간을 1 증가
             now += 1
-            
-    return answer // jobCnt #나머지는 버려
+    
+    # 모든 작업을 완료하는데 걸린 총 시간의 평균 반환
+    return answer // jobCnt
