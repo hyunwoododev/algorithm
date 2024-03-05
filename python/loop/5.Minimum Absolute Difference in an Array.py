0d2ba4e06e4b51efe4790c2d1c6c3c37012afb1b
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-03-05 20:06:04 +0900

    feat: stacking

--- python/loop/5.Minimum Absolute Difference in an Array.py
+++ python/loop/5.Minimum Absolute Difference in an Array.py
@@ -1,14 +1,15 @@
 # https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem?isFullScreen=true
 
-n = int(input().strip())  # 원소의 개수 입력
-a = list(map(int, input().strip().split(' ')))  # 원소 입력
-a.sort()  # 배열을 오름차순으로 정렬
-min_diff = a[n-1] - a[0]  # 최대값으로 설정
-
-# 배열을 순회하면서 인접한 원소들 간의 차이를 계산
-for i in range(1, n):
-    min_diff = min(a[i] - a[i-1], min_diff)  # 최소 차이 업데이트
-    
-print(min_diff)  # 최소 차이 출력
+n = int(input().strip()) 
+a = list(map(int, input().strip().split(' ')))
 
 # 두수의 abs란 무엇이가. -> 두 수의 차이를 보고싶은거다.
+a.sort()
+
+min_diff = a[n-1] - a[0]
+
+for i in range(1, n):
+    min_diff = min(a[i] - a[i-1], min_diff)
+    
+print(min_diff)
+
---
+++ python/loop/9.Valid Anagram.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        match = 0
+        sCount = [0] * 26
+        tCount = [0] * 26
+        answer = True
+
+        if len(s) != len(t):
+            return False
+            
+        for i in range(len(s)):
+            tCount[ord(t[i]) - ord('a')] += 1
+            sCount[ord(s[i]) - ord('a')] += 1
+        
+        for i in range(len(s)):
+            index = ord(s[i]) - ord('a')
+            if tCount[index] != sCount[index]:
+                return False
+
+        return answer
+
+
+        
