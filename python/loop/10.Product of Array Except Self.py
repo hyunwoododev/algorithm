83cba04340fc7224a9c28e7d4850fc3dfafd763b
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-03-06 01:07:54 +0900

    Update 10.Product of Array Except Self.py

--- python/loop/10.Product of Array Except Self.py
+++ python/loop/10.Product of Array Except Self.py
@@ -2,12 +2,17 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        leftRes = [1]*len(nums)
+        rightRes =[1]*len(nums)
 
-        for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+        for i in range(1,len(nums)):
+            leftRes[i] = leftRes[i-1] * nums[i-1]
+        
+        for j in range(len(nums)-2, -1, -1):
+            rightRes[j] = rightRes[j+1] * nums[j+1]
+        
+        answer = []
+        for i in range(len(nums)):
+            answer.append(leftRes[i] * rightRes[i])
+            
+        return answer
