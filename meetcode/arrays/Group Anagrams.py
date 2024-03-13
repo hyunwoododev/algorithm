a8e9be315063b39b7e9d37f5e7cfdbc10468bb5b
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-03-13 14:10:20 +0900

    feat: start meet code

---
+++ meetcode/arrays/Group Anagrams.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/group-anagrams/description/
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
