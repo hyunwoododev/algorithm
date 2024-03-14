c2d730b15d1107f5b4bfb70a4b46db91ce233144
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-03-14 16:35:36 +0900

    feat: remove unused code

--- js/포켓몬.js
+++
@@ -1,8 +0,0 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=javascript
-
-function solution(nums) {
-  let getNum = nums.length / 2;
-  let result = [...new Set(nums)]; // 중복을 제거하여 폰켓몬의 종류만 남깁니다.
-  let answer = result.length > getNum ? getNum : result.length;
-  return answer;
-}
