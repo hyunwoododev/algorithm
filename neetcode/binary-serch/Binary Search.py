bffab5646ddb2fdbee2c6a7c529de571a33dd913
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-03-15 17:39:51 +0900

    feat: confused

---
+++ neetcode/binary-serch/Binary Search.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/binary-search/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            # (l + r) // 2 can lead to overflow
+            m = l + ((r - l) // 2)  
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
---
+++ neetcode/binary-serch/Search a 2D Matrix.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/search-a-2d-matrix/description/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+
+        top = 0
+        bot = ROWS - 1
+        
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+                
+        if top > bot:
+            return False  
+        
+        row = (top + bot) // 2
+
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+            
+        return False
