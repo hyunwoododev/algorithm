077286882bb98a20510a3cbd02962d772c6746e0
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-03-20 15:32:09 +0900

    feat: heapq

--- neetcode/heap/Kth Largest Element In An Array.py
+++ neetcode/heap/Kth Largest Element In An Array.py
@@ -1,2 +1,14 @@
 # https://leetcode.com/problems/kth-largest-element-in-an-array/
 
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, -num)
+        
+        answer = None
+        while heap and k > 0:
+            answer = heapq.heappop(heap)
+            k -= 1
+        return answer * -1
---
+++ neetcode/heap/Task Scheduler.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/task-scheduler/description/
+
+import heapq
+from collections import Counter, deque
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)  
+
+        time = 0 
+        q = deque()
+
+        while maxHeap or q:
+            time += 1
+
+            if not maxHeap:
+                time = q[0][1]
+
+            else:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+
+            if q and q[0][1] == time: 
+                heapq.heappush(maxHeap, q.popleft()[0])
+                
+        return time 
+
