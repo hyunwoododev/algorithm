63479581f79a8d784a457b81128fb19d62d34de4
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-04-02 14:36:17 +0900

    feat: bfs init

--- neetcode/greedy/Gas Station.py
+++ neetcode/greedy/Gas Station.py
@@ -6,9 +6,9 @@ class Solution:
             return  -1
 
         total = 0
-        res = 0
+        res = 0 
         for i in range(len(gas)):
-            total = gas[i] - cost[i]
+            total += (gas[i] - cost[i])
             if total < 0:
                 total = 0
                 res = i + 1
