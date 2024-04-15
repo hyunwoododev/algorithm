37e1183b39355ea28975d7ee90dfa75ba830ce06
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-04-15 19:52:02 +0900

    feat: dfs

--- neetcode/backtacking/N-Queens.py
+++ neetcode/backtacking/N-Queens.py
@@ -2,22 +2,20 @@
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col = set()        # 각 열에 퀸이 위치하는지를 나타내는 집합
-        posDiag = set()   # 양 대각선 방향에 퀸이 위치하는지를 나타내는 집합 (r + c)
-        negDiag = set()   # 음 대각선 방향에 퀸이 위치하는지를 나타내는 집합 (r - c)
+        col = set()        
+        posDiag = set()  
+        negDiag = set() 
 
-        res = []          # 결과를 저장할 리스트
-        board = [["."] * n for i in range(n)]  # 체스판을 나타내는 이차원 리스트
+        res = []
+        board = [["."] * n for i in range(n)] 
 
-        # 백트래킹을 통해 가능한 퀸의 배치를 찾는 함수
         def backtrack(r):
-            if r == n:  # 퀸을 모두 배치했을 때 결과를 저장하고 종료
+            if r == n: 
                 copy = ["".join(row) for row in board]
                 res.append(copy)
                 return
 
             for c in range(n):
-                # 현재 열, 양 대각선, 음 대각선 상에 퀸이 위치하는지 확인
                 if c in col or (r + c) in posDiag or (r - c) in negDiag:
                     continue
 
@@ -34,7 +32,6 @@ class Solution:
                 col.remove(c)
                 posDiag.remove(r + c)
                 negDiag.remove(r - c)
-
                 board[r][c] = "."
 
         backtrack(0)  # 백트래킹 시작
--- neetcode/backtacking/Word Search.py
+++ neetcode/backtacking/Word Search.py
@@ -1,56 +1,44 @@
 # https://leetcode.com/problems/word-search/description/
 
-from collections import defaultdict, Counter
-
 class Solution:
-    # 주어진 보드에서 단어가 존재하는지 수평 또는 수직으로 탐색하여 확인하는 함수.
     def exist(self, board: List[List[str]], word: str) -> bool:
-        # 보드의 행과 열의 크기를 구함
-        ROWS, COLS = len(board), len(board[0])
-        
-        # 방문한 위치를 추적하기 위한 집합
+        maxRow = len(board)
+        maxCol = len(board[0]) 
         path = set()
 
-        # 단어의 존재 여부를 확인하기 위한 깊이 우선 탐색(DFS) 함수
-        def dfs(r, c, i):
-            # 기본 사례: 전체 단어를 찾은 경우
-            if i == len(word):
+        def dfs( r, c, idx):
+            if idx == len(word):
                 return True
-            
-            # 범위를 벗어나거나 문자가 일치하지 않거나 위치가 이미 방문한 경우 False 반환
+
             if (
-                min(r, c) < 0
-                or r >= ROWS
-                or c >= COLS
-                or word[i] != board[r][c]
-                or (r, c) in path
+                # out of range
+                c < 0 
+                or r < 0
+                or r >= maxRow 
+                or c >= maxCol 
+
+                # not correct char
+                or board[r][c] != word[idx] or
+                (r,c) in path
             ):
                 return False
             
-            # 현재 셀을 방문한 것으로 표시
-            path.add((r, c))
+            path.add((r,c))
 
-            # 네 방향으로 재귀적으로 탐색
             res = (
-                dfs(r + 1, c, i + 1)
-                or dfs(r - 1, c, i + 1)
-                or dfs(r, c + 1, i + 1)
-                or dfs(r, c - 1, i + 1)
+                dfs(r+1, c, idx+1) or
+                dfs(r-1, c, idx+1) or
+                dfs(r, c+1, idx+1) or
+                dfs(r, c-1, idx+1)
             )
 
-            # 백트래킹을 위해 현재 셀을 방문한 집합에서 제거
-            path.remove((r, c))
-            return res
+            path.remove((r,c))
 
-        # 보드 내 각 문자의 빈도를 계산
-        count = defaultdict(int, sum(map(Counter, board), Counter()))
-        # 단어의 첫 글자보다 마지막 글자의 빈도가 더 낮은 경우, 효율성을 위해 검색 방향을 반대로 함
-        if count[word[0]] > count[word[-1]]:
-            word = word[::-1]
-            
-        # 각 셀에서 DFS 시작
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0):
+            return res
+        
+        for r in range(maxRow):
+            for c in range(maxCol):
+                if dfs(r,c,0):
                     return True
+
         return False
