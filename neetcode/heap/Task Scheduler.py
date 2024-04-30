2c6cdc7925282d3775564816be28ba5a9e94f952
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-04-30 16:09:12 +0900

    feat: task scheduler

--- neetcode/heap/Task Scheduler.py
+++ neetcode/heap/Task Scheduler.py
@@ -1,27 +1,24 @@
 # https://leetcode.com/problems/task-scheduler/description/
 
+from collections import deque,Counter
 import heapq
-from collections import Counter, deque
-
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxHeap)  
-
-        time = 0
+        # heap 
+        heap =[-i for i in count.values()]
+        heapq.heapify(heap)
+        
+        # q
         q = deque()
-        while maxHeap or q:
-            time += 1
-            
-            if not maxHeap:
-                time = q[0][1]
-            else:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time + n])
-
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
-                
-        return time # 모든 태스크가 완료되는 데 필요한 최소 시간 반환
+        time = 0
+        while q or heap:
+            if heap:
+                value = heapq.heappop(heap) * -1
+                value = value - 1
+                if value > 0:
+                    q.append((value, time+n))
+            while q and q[0][1] <= time:
+                heapq.heappush(heap,q.popleft()[0] * -1)     
+            time+=1
+        return time
