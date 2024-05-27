fb872e07055fa1dd880e750423bc8b4fc7d15d7f
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-05-27 15:08:48 +0900

    feat: union find class

--- hackerrank/graph/Journey to the Moon.py
+++ hackerrank/graph/Journey to the Moon.py
@@ -2,10 +2,8 @@
 
 class UnionFind:
     def __init__(self, n):
-        # `n`개의 요소로 초기화된 Union-Find 자료 구조를 초기화합니다.
         self.parent = list(range(n))  # 초기에는 각 노드가 자신의 부모입니다.
         self.rank = [1] * n  # 트리를 평평하게 유지하기 위해 랭크를 사용합니다.
-        self.size = [1] * n  # 각 구성 요소의 크기를 추적하기 위한 크기 배열입니다.
 
     def find(self, u):
         if self.parent[u] != u:
@@ -20,31 +18,34 @@ class UnionFind:
         if root_u != root_v:
             if self.rank[root_u] > self.rank[root_v]:
                 self.parent[root_v] = root_u
-                self.size[root_u] += self.size[root_v]
             elif self.rank[root_u] < self.rank[root_v]:
                 self.parent[root_u] = root_v
-                self.size[root_v] += self.size[root_u]
             else:
                 self.parent[root_v] = root_u
-                self.size[root_u] += self.size[root_v]
                 self.rank[root_u] += 1
-
-    def get_size(self, u):
-        # 노드 `u`가 속한 집합의 크기를 반환합니다.
-        return self.size[self.find(u)]
 
 def journeyToMoon(n, astronaut):
     uf = UnionFind(n)
-    
+    # 각 집합의 크기를 추적할 리스트를 초기화합니다.
+    size = [1] * n
+
     # 각 우주비행사 쌍에 대해 유니온 연산을 수행합니다.
     for u, v in astronaut:
-        uf.union(u, v)
+        root_u = uf.find(u)
+        root_v = uf.find(v)
+        if root_u != root_v:
+            # 두 집합을 합치고, 합친 집합의 크기를 업데이트합니다.
+            if uf.union(u, v):
+                new_root = uf.find(u)
+                if new_root == root_u:
+                    size[root_u] += size[root_v]
+                    size[root_v] = 0
+                else:
+                    size[root_v] += size[root_u]
+                    size[root_u] = 0
     
     # 각 국가의 크기를 추적합니다.
-    country_sizes = []
-    for i in range(n):
-        if uf.find(i) == i:
-            country_sizes.append(uf.get_size(i))
+    country_sizes = [size[i] for i in range(n) if uf.find(i) == i]
     
     # 가능한 모든 페어의 수를 계산합니다.
     total_pairs = n * (n - 1) // 2
--- leetcode/graphs/Redundant Connection.py
+++ leetcode/graphs/Redundant Connection.py
@@ -1,31 +1,37 @@
 # https://leetcode.com/problems/redundant-connection/description/
 
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def getParent(self, node):
+        if self.parent[node] == node:
+            return node
+        self.parent[node] = self.getParent(self.parent[node])
+        return self.parent[node]
+
+    def union(self, node1, node2):
+        parent1 = self.getParent(node1)
+        parent2 = self.getParent(node2)
+        
+        if parent1 != parent2:
+            if self.rank[parent1] > self.rank[parent2]:
+                self.parent[parent2] = parent1
+            elif self.rank[parent1] < self.rank[parent2]:
+                self.parent[parent1] = parent2
+            else:
+                self.parent[parent2] = parent1
+                self.rank[parent1] += 1
+
+    def find(self, node1, node2):
+        return self.getParent(node1) == self.getParent(node2)
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        par = [i for i in range(len(edges) + 1)]
-        rank = [1] * (len(edges) + 1)
-
-        def find(n):
-            p = par[n]
-            while p != par[p]:
-                par[p] = par[par[p]]
-                p = par[p]
-            return p
-
-        # return False if already unioned
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-
-            if p1 == p2:
-                return False
-            if rank[p1] > rank[p2]:
-                par[p2] = p1
-                rank[p1] += rank[p2]
+        uf = UnionFind(len(edges) + 1)
+        for a, b in edges:
+            if uf.find(a, b):
+                return [a, b]
             else:
-                par[p1] = p2
-                rank[p2] += rank[p1]
-            return True
-
-        for n1, n2 in edges:
-            if not union(n1, n2):
-                return [n1, n2]
+                uf.union(a, b)
