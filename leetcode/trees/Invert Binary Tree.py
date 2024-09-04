1abb1adaf49ed878401ae8ce9823d93caf63976a
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-09-04 22:31:49 +0900

    fea: tree

--- leetcode/trees/Invert Binary Tree.py
+++ leetcode/trees/Invert Binary Tree.py
@@ -6,6 +6,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
