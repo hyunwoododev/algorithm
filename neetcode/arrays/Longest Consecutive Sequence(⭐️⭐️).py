84e8da6bd853a26d2208c0a48a60d334daff406f
Hyunnwoodo <121751640+hyunwoododev@users.noreply.github.com>
2024-10-10 17:04:07 +0900

    Update Longest Consecutive Sequence(⭐️⭐️).py

--- neetcode/arrays/Longest Consecutive Sequence(⭐️⭐️).py
+++ neetcode/arrays/Longest Consecutive Sequence(⭐️⭐️).py
@@ -1,10 +1,12 @@
 # https://leetcode.com/problems/longest-consecutive-sequence/description/
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        
         numSet = set(nums)
         longest = 0
 
         for n in numSet:
+            # n-1 탐색이 만약에 리스트였다면 O(N)인데 set이어서 n-1 찾는게 O(1)
             if (n - 1) not in numSet:
                 length = 1
                 while (n + length) in numSet:
