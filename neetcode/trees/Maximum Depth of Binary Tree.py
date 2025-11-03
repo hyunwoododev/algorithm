ec8ff4faff8dbed8faa705df4c439b202d01403e
dohyunwoo-gint <hyunwoo.do@gintlab.com>
2025-11-03 17:10:18 +0900

    ㅇ

--- neetcode/trees/Maximum Depth of Binary Tree.py
+++ neetcode/trees/Maximum Depth of Binary Tree.py
@@ -1,56 +1,14 @@
-# https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
-
-from collections import deque
-from typing import Optional
-
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-# RECURSIVE DFS
+
 class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        if not root:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
             return 0
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
-
-# ITERATIVE DFS
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        stack = [[root, 1]]
-        res = 0
-
-        while stack:
-            node, depth = stack.pop()
-
-            if node:
-                res = max(res, depth)
-                stack.append([node.left, depth + 1])
-                stack.append([node.right, depth + 1])
-        return res
-
-
-# BFS
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        q = deque()
-        if root:
-            q.append(root)
-
-        level = 0
-
-        while q:
-
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        return level
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
