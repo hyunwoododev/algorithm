cf6a2a5e8c7e17f63b3514a2437349492691da27
dohyunwoo-gint <hyunwoo.do@gintlab.com>
2025-11-03 16:37:44 +0900

    d

--- programmers/4.py
+++ programmers/4.py
@@ -1,16 +1,17 @@
 from collections import defaultdict, deque
 
+
 def can_process_request(requests):
     # 도메인별로 요청 시간 인덱스를 저장할 해시맵
     domain_requests = defaultdict(deque)
     results = []
-    
+
     # 각 요청에 대해 순차적으로 처리
     for index, request in enumerate(requests):
         # 오래된 요청을 제거
         while domain_requests[request] and index - domain_requests[request][0] >= 30:
             domain_requests[request].popleft()
-        
+
         # recent_5s 리스트 초기화 및 구성
         recent_5s = []
         for t in domain_requests[request]:
@@ -23,13 +24,24 @@ def can_process_request(requests):
             results.append("(status: 200, message: ok!)")
         else:
             results.append("(status: 429, message: Too many requests)")
-    
+
     return results
 
-# 예제 입력
-requests = ["www.xyz.com", "www.abc.com", "www.xyz.com", "www.pqr.com", 
-            "www.abc.com", "www.xyz.com", "www.xyz.com", "www.abc.com", "www.xyz.com"]
 
+# 예제 입력
+requests = [
+    "www.xyz.com",
+    "www.abc.com",
+    "www.xyz.com",
+    "www.pqr.com",
+    "www.abc.com",
+    "www.xyz.com",
+    "www.xyz.com",
+    "www.abc.com",
+    "www.xyz.com",
+]
+
+# test
 # 함수 실행 및 결과 출력
 result = can_process_request(requests)
 print(result)
