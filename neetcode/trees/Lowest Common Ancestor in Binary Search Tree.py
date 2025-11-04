41c296c351d0edd65052b756594eb5ace6d973a0
dohyunwoo-gint <hyunwoo.do@gintlab.com>
2025-11-04 11:14:22 +0900

    Update Lowest Common Ancestor in Binary Search Tree.py

--- neetcode/trees/Lowest Common Ancestor in Binary Search Tree.py
+++ neetcode/trees/Lowest Common Ancestor in Binary Search Tree.py
@@ -2,6 +2,7 @@
 
 from typing import Optional
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -9,8 +10,11 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    def lowestCommonAncestor(
+        self, root: TreeNode, p: TreeNode, q: TreeNode
+    ) -> TreeNode:
         while True:
             if root.val < p.val and root.val < q.val:
                 root = root.right
