ed628eeda09cd36ac02b45e2d3dff625800d7ce6
dohyunwoo-gint <hyunwoo.do@gintlab.com>
2025-11-06 18:15:01 +0900

    ㄹ

--- neetcode/graphs/Graph Valid Tree.py
+++ neetcode/graphs/Graph Valid Tree.py
@@ -1,75 +1,28 @@
 # https://neetcode.io/problems/valid-tree
 
-# Problem is free on Lintcode
-class Solution:
-    """
-    @param n: An integer
-    @param edges: a list of undirected edges
-    @return: true if it's a valid tree, or false
-    """
 
-    def validTree(self, n, edges):
-        if not n:
-            return True
-        adj = {i: [] for i in range(n)}
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > (n - 1):
+            return False
+
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
 
         visit = set()
 
-        def dfs(i, prev):
-            if i in visit:
+        def dfs(node, par):
+            if node in visit:
                 return False
 
-            visit.add(i)
-            for j in adj[i]:
-                if j == prev:
+            visit.add(node)
+            for nei in adj[node]:
+                if nei == par:
                     continue
-                if not dfs(j, i):
+                if not dfs(nei, node):
                     return False
             return True
 
-        return dfs(0, -1) and n == len(visit)
-    
-    
-    
-# alternative solution via DSU O(ElogV) time complexity and 
-# save some space as we don't recreate graph\tree into adjacency list prior dfs and loop over the edge list directly
-class Solution:
-    """
-    @param n: An integer
-    @param edges: a list of undirected edges
-    @return: true if it's a valid tree, or false
-    """
-    def __find(self, n: int) -> int:
-        while n != self.parents.get(n, n):
-            n = self.parents.get(n, n)
-        return n
-
-    def __connect(self, n: int, m: int) -> None:
-        pn = self.__find(n)
-        pm = self.__find(m)
-        if pn == pm:
-            return
-        if self.heights.get(pn, 1) > self.heights.get(pm, 1):
-            self.parents[pn] = pm
-        else:
-            self.parents[pm] = pn
-            self.heights[pm] = self.heights.get(pn, 1) + 1
-        self.components -= 1
-
-    def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
-        # init here as not sure that ctor will be re-invoked in different tests
-        self.parents = {}
-        self.heights = {}
-        self.components = n
-
-        for e1, e2 in edges:
-            if self.__find(e1) == self.__find(e2):  # 'redundant' edge
-                return False
-            self.__connect(e1, e2)
-
-        return self.components == 1  # forest contains one tree
-
-
+        return dfs(0, -1) and len(visit) == n
--- neetcode/graphs/Is Graph Bipartite.py
+++ neetcode/graphs/Is Graph Bipartite.py
@@ -3,17 +3,18 @@
 from collections import deque
 from typing import List
 
+
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         colors = [0] * len(graph)
         for i in range(len(graph)):
             if colors[i] != 0:
                 continue
-            
+
             queue = deque()
             queue.append(i)
             colors[i] = 1
-            
+
             while queue:
                 node = queue.popleft()
                 for neighbor in graph[node]:
@@ -25,4 +26,3 @@ class Solution:
                         # 인접한 정점이 같은 색이라면, 이분 그래프가 아님
                         return False
         return True
-                 
