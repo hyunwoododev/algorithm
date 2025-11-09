500c5a37358d0a9b697758863f2a9dd543fe684f
Hyunnwoodo <121751640+hyunwoododev@users.noreply.github.com>
2025-11-09 21:32:20 +0900

    feat: go training

---
+++ neetcode/go/Contains Duplicate.go
@@ -0,0 +1,12 @@
+// https://neetcode.io/problems/duplicate-integer?list=neetcode150
+
+func hasDuplicate(nums []int) bool {
+	seen := make(map[int]bool)
+	for _, num := range nums {
+		if seen[num] {
+			return true
+		}
+		seen[num] = true
+	}
+	return false
+}
---
+++ neetcode/go/Valid Anagram.go
@@ -0,0 +1,20 @@
+// https://neetcode.io/problems/is-anagram?list=neetcode150
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	countS, countT := make(map[rune]int), make(map[rune]int)
+	for i, ch := range s {
+		countS[ch]++
+		countT[rune(t[i])]++
+	}
+
+	for k, v := range countS {
+		if countT[k] != v {
+			return false
+		}
+	}
+	return true
+}
